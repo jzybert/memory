318077a50c197a16f6804e9db98cd75380c44243
jzybert <jmzybert@gmail.com>
2018-09-23 14:44:32 -0400

    Add correct server name

--- start.sh
+++ start.sh
@@ -9,10 +9,5 @@ _build/prod/rel/memory/bin/memory stop || true
 
 echo "Starting app..."
 
-# TODO: You want start.
-
 _build/prod/rel/memory/bin/memory start
-
-# TODO: Add a cron rule or systemd service file
-#       to start your app on system boot.
 
