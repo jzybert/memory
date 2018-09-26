0466ac64e1f039a08bede91bb1d8175a4d137f94
jzybert <jmzybert@gmail.com>
2018-09-26 13:41:36 -0400

    Turn tile into a stateless component

--- assets/js/tile.jsx
+++ assets/js/tile.jsx
@@ -1,52 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Tile extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      index: props.index,
-      matchFound: props.matchFound,
-      showLetter: props.showLetter,
-      letterDisplayed: props.letterDisplayed
-    };
-
-    this.toggleLetter = this.toggleLetter.bind(this);
-    this.revealLetter = this.revealLetter.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      matchFound: nextProps.matchFound,
-      showLetter: nextProps.showLetter,
-      letterDisplayed: nextProps.letterDisplayed
-    })
-  }
-
-  toggleLetter() {
-    if (!this.state.matchFound) {
-      this.revealLetter();
+const Tile = ({index, matchFound, showLetter, letterDisplayed, handleOnClick}) => {
+  const revealLetter = () => {
+    if (!matchFound) {
+      handleOnClick(index);
     }
-  }
-
-  revealLetter() {
-    this.props.handleOnClick(this.props.index);
   };
 
-  render() {
-    const fill = this.state.matchFound ? 'green' : (this.state.showLetter ? 'white' : 'lightgrey');
-    const letterColor = this.state.matchFound ? 'white' : 'black';
-    let letter = this.state.showLetter ? <div style={{color: letterColor}}>{this.state.letterDisplayed}</div> : null;
+  const render = () => {
+    const fill = matchFound ? 'green' : (showLetter ? 'white' : 'lightgrey');
+    const letterColor = matchFound ? 'white' : 'black';
+    let letter = showLetter ? <div style={{color: letterColor}}>{letterDisplayed}</div> : null;
 
     return (
-      <div onClick={this.toggleLetter} style={{
+      <div onClick={revealLetter} style={{
         backgroundColor: fill,
         width: '100px',
         height: '100px',
         border: '2px solid black',
         cursor: 'pointer',
         margin: '20px'
-       }}>
+      }}>
         <div style={{
           textAlign: 'center',
           width: '100%',
@@ -57,5 +31,9 @@ export default class Tile extends Component {
         </div>
       </div>
     );
-  }
-}
+  };
+
+  return render();
+};
+
+export default Tile;
